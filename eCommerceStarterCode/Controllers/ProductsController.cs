2efbc74a8f08af861815a7c43ecdf6be2023df54
Ebony Riddick <ebonyriddick@gmail.com>
2021-11-22 16:27:50 -0500

    modified controller

---
+++ eCommerceStarterCode/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET ALL <baseurl>/api/product
+        [HttpGet]
+        public IActionResult Get()
+        {
+            //Retrieves all products from db logic
+            var products = _context.Products;
+            return Ok(products);
+        }
+        // POST <baseurl>/api/product
+        [HttpPost]
+        public IActionResult Post([FromBody] Product value)
+        {
+            _context.Products.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+    }
+}
