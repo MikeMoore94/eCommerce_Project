3f8bd22e24e46f5c58453cf4dbea3cd5c90fdb47
KasebrotII <kasebrot@live.com>
2021-11-24 10:46:54 -0600

    added methods to product controller

--- eCommerceStarterCode/Controllers/ProductsController.cs
+++ eCommerceStarterCode/Controllers/ProductsController.cs
@@ -19,21 +19,41 @@ namespace eCommerceStarterCode.Controllers
         {
             _context = context;
         }
-        // GET ALL <baseurl>/api/product
+        // GET ALL
         [HttpGet]
         public IActionResult Get()
         {
-            //Retrieves all products from db logic
             var products = _context.Products;
             return Ok(products);
         }
-        // POST <baseurl>/api/product
+        // GET
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            var merch = _context.Products.Find(id);
+            if (merch == null)
+            {
+                return NotFound();
+            }
+            return Ok(merch);
+        }
+        // POST 
         [HttpPost]
         public IActionResult Post([FromBody] Product value)
         {
             _context.Products.Add(value);
             _context.SaveChanges();
             return StatusCode(201, value);
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            var product = _context.Products.Find(id);
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return Ok(product);
         }
     }
 }
